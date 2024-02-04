0b3bd2bfa35b17d6e15c907c99bc67a9a6949f2b
Umer_pogrammer <78730094+umerprogramm@users.noreply.github.com>
2024-02-04 15:30:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 app.use(bodyParser.json())
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
